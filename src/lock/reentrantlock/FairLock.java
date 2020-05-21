2416ec7ce29338494644470353a8b2671ddc87f1
zhangbiao <zhangbiao97@126.com>
2020-05-21 17:27:17 +0800

    公平锁与非公平锁

---
+++ src/lock/reentrantlock/FairLock.java
@@ -0,0 +1,77 @@
+package lock.reentrantlock;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 描述：演示公平和不公平两种情况
+ * <p>
+ * Create By ZhangBiao
+ * 2020/5/21
+ */
+public class FairLock {
+
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread[] thread = new Thread[10];
+        for (int i = 0; i < thread.length; i++) {
+            thread[i] = new Thread(new Job(printQueue));
+        }
+        for (int i = 0; i < thread.length; i++) {
+            thread[i].start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+class Job implements Runnable {
+
+    PrintQueue printQueue;
+
+    public Job(PrintQueue printQueue) {
+        this.printQueue = printQueue;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "开始打印");
+        printQueue.printJob(new Object());
+        System.out.println(Thread.currentThread().getName() + "打印完毕");
+    }
+
+}
+
+class PrintQueue {
+
+    private Lock queueLock = new ReentrantLock(false);
+
+    public void printJob(Object document) {
+        queueLock.lock();
+        try {
+            int duration = new Random().nextInt(10) + 1;
+            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration + "秒");
+            Thread.sleep(duration * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+        queueLock.lock();
+        try {
+            int duration = new Random().nextInt(10) + 1;
+            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration + "秒");
+            Thread.sleep(duration * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+
+}
---
+++ src/lock/reentrantlock/GetHoldCount.java
@@ -0,0 +1,29 @@
+package lock.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Create By ZhangBiao
+ * 2020/5/21
+ */
+public class GetHoldCount {
+
+    static ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.lock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+        lock.unlock();
+        System.out.println(lock.getHoldCount());
+    }
+
+}

1632c5d0de78e9fa3df5be200bf34885fe2649d6
zhangbiao <zhangbiao97@126.com>
2020-05-27 12:58:23 +0800

    并发流程控制工具

---
+++ src/flowcontrol/condition/ConditionDemo1.java
@@ -0,0 +1,57 @@
+package flowcontrol.condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 描述：演示Condition的基本用法
+ * <p>
+ * Create By ZhangBiao
+ * 2020/5/27
+ */
+public class ConditionDemo1 {
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    private Condition condition = lock.newCondition();
+
+    void method1() {
+        lock.lock();
+        try {
+            System.out.println("条件不满足，开始await");
+            condition.await();
+            System.out.println("条件满足了，开始执行后续任务");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void method2() {
+        lock.lock();
+        try {
+            System.out.println("准备工作完成，唤醒其他线程");
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConditionDemo1 conditionDemo1 = new ConditionDemo1();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    conditionDemo1.method2();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        conditionDemo1.method1();
+    }
+
+}
---
+++ src/flowcontrol/semaphore/SemaphoreDemo.java
@@ -0,0 +1,46 @@
+package flowcontrol.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 描述：演示Semaphore用法
+ * <p>
+ * Create By ZhangBiao
+ * 2020/5/27
+ */
+public class SemaphoreDemo {
+
+    static Semaphore semaphore = new Semaphore(5, true);
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 100; i++) {
+            service.submit(new Task());
+        }
+        service.shutdown();
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release();
+        }
+
+    }
+
+}
